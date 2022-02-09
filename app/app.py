53148d87eac14dbb6318a69283ad664453fa4368
AnnDevOpsAccount <anndavey.uk@gmail.com>
2022-02-09 15:24:58 +0000

    1st success

--- app/app.py
+++ app/app.py
@@ -1,12 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    image_x = get_picture()
+    return render_template('index.html', landing_image=image_x)
 
-@app.route('/mars')
+
+@app.route('/mars' , methods=['POST'])
 def mars():
-    return render_template('mars.html')
+    #itemTitle = request.form.get('item-input')
+    #add_item(itemTitle)        
+    #return redirect(url_for('index'))
+    
+
+
+
+    image_x = 'https://upload.wikimedia.org/wikipedia/commons/4/47/American_Eskimo_Dog.jpg'
+    
+    #return redirect(url_for('index'))
+    return (image_x)
+
+def get_picture():
+
+    url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+
+    querystring = {"sol":"1000","camera":"fhaz","api_key":"KXaVsIhdF4hYReZ1rMvWTqMw1m4OrAvd0idOLBq9"}
+
+    response = requests.request("GET", url, params=querystring)
+    resp = response.json()
+    photos = resp['photos']
+    
+    return photos[0]['img_src']
